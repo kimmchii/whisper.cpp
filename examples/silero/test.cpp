01a0fb070cbb6f00185a5ae151c45349fb8fe83c
kimmchii <thanachotkim@hotmail.co.th>
2023-11-30 15:51:33 +0700

    adds .cpp code for silero - voice activity detection

---
+++ examples/silero/test.cpp
@@ -0,0 +1,11 @@
+#include "silero-vad.h"
+
+int main(int argc, char *argv[]){
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <audio_file_path>" << std::endl;
+        return 1;
+    }
+    std::string audio_file_path = argv[1];
+    perform_vad(audio_file_path);
+}
